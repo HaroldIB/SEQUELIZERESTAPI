ad77853ab4e4f1756773cba7a5b0e6a419eea3aa
Harold <haroldbustamante22@gmail.com>
2022-05-18 16:33:07 -0400

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import express from "express";
+import projectsRoutes from "./routes/projects.routes.js";
+const app = express();
+app.use(projectsRoutes);
+export default app;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import app from "./app.js";
+import { sequelize } from "./database/database.js";
+// import './models/Project.js';
+// import './models/Task.js';
+async function main() {
+    try {
+        await sequelize.sync({ force: false });
+        app.listen(4000);
+        console.log('Server is listening in port: ', 4000);
+    } catch (error) {
+        console.log('Unable to connect to the database: ', error);
+    }
+
+}
+
+main();
